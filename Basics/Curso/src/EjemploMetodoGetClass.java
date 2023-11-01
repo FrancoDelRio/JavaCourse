8482164a8c26174bf4f10a7c28ec9be64a554004
FrancoDelRio <francodelrio10@gmail.com>
2023-10-31 22:06:39 -0500

    Finalization of the topic of the getClass() method and the super clases that englobes all the clases in Java

--- Basics/Curso/src/EjemploMetodoGetClass.java
+++ Basics/Curso/src/EjemploMetodoGetClass.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Method;
+
 public class EjemploMetodoGetClass {
     public static void main(String[] args) {
 
@@ -8,6 +10,22 @@ public class EjemploMetodoGetClass {
         System.out.println("strClass.getName() = " + strClass.getName());
         System.out.println("strClass.getSimpleName() = " + strClass.getSimpleName());
         System.out.println("strClass.getPackageName() = " + strClass.getPackageName());
-        System.out.println("strClass = " + strClass);
+        System.out.println("strClass = " + strClass + "\n");
+
+        for (Method method: strClass.getMethods()){
+            System.out.println("metodo.getName() = " + method.getName());
+        }
+        System.out.println("\n");
+
+        Integer num = 34;
+        Class intClass = num.getClass();
+        Class objClass = intClass.getSuperclass().getSuperclass();
+        System.out.println("intClass.getSuperClass() = " + objClass);
+
+        for (Method method: objClass.getMethods()){
+            System.out.println("method.getName() = " + method.getName() );
+        }
+
+
     }
 }
